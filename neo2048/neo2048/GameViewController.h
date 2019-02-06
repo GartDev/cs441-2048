d38ffc410d87cb04527f4b442f7e6ac326e631fb
Garrett Vanderlinden <gvander4@bl-n20.pods.bu.int>
2019-02-06 15:18:26 -0500

    Updated UI to look not awful and added swipe gesture recognizer

--- neo2048/neo2048/GameViewController.h
+++ neo2048/neo2048/GameViewController.h
@@ -11,5 +11,22 @@
 #import <GameplayKit/GameplayKit.h>
 
 @interface GameViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *sqr00;
+@property (strong, nonatomic) IBOutlet UILabel *sqr10;
+@property (strong, nonatomic) IBOutlet UILabel *sqr20;
+@property (strong, nonatomic) IBOutlet UILabel *sqr30;
+@property (strong, nonatomic) IBOutlet UILabel *sqr01;
+@property (strong, nonatomic) IBOutlet UILabel *sqr11;
+@property (strong, nonatomic) IBOutlet UILabel *sqr21;
+@property (strong, nonatomic) IBOutlet UILabel *sqr31;
+@property (strong, nonatomic) IBOutlet UILabel *sqr02;
+@property (strong, nonatomic) IBOutlet UILabel *sqr12;
+@property (strong, nonatomic) IBOutlet UILabel *sqr22;
+@property (strong, nonatomic) IBOutlet UILabel *sqr32;
+@property (strong, nonatomic) IBOutlet UILabel *sqr03;
+@property (strong, nonatomic) IBOutlet UILabel *sqr13;
+@property (strong, nonatomic) IBOutlet UILabel *sqr23;
+@property (strong, nonatomic) IBOutlet UILabel *sqr33;
+@property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *swiper;
 
 @end
